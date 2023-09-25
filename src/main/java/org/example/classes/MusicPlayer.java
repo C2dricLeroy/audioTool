0d2adf16f158ce79b08c9ad3e7d95bdf847da6e8
Cdric2Leroy <cedric.leroy.psycho@gmail.com>
2023-09-25 17:03:03 +0100

    feat(MusicPlayer): add a MusicPlayer class

---
+++ src/main/java/org/example/classes/MusicPlayer.java
@@ -0,0 +1,65 @@
+package org.example.classes;
+
+
+
+public class MusicPlayer {
+
+    private Playlist currentPlaylist;
+    private Audiocontrol audioControl;
+    private boolean isPlaylistShuffle;
+
+
+    public void play() {
+
+    }
+
+    public void pause() {
+
+    }
+
+    public void stop() {
+
+    }
+
+    public void next() {
+
+    }
+
+    public void previous() {
+
+    }
+
+    public void addToPlaylist(Song song) {
+
+    }
+
+    public void removeFromPlaylist(Song song) {
+
+    }
+
+    public void loadPlaylist(Playlist playlist) {
+
+    }
+
+    public void clearPlaylist() {
+        this.currentPlaylist.clearPlaylist();
+    }
+
+    public void shufflePlaylist() {
+        this.isPlaylistShuffle = !this.isPlaylistShuffle;
+    }
+
+    public boolean isPlaying() {
+        return true;
+    }
+
+    public Song getCurrentSong() {
+        return new Song("test", 12, "test");
+    }
+
+
+
+
+
+
+}
