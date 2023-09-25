79737fab1308534c35a67be573ea14175fb555d1
Cdric2Leroy <cedric.leroy.psycho@gmail.com>
2023-09-25 17:17:03 +0100

    feat(DatabaseConnection): add the DatabaseConnection class

---
+++ src/main/java/org/example/classes/DatabaseConnection.java
@@ -0,0 +1,48 @@
+package org.example.classes;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DatabaseConnection {
+    private static final String DATABASE_URL = "";
+    private Connection connection;
+
+    public static DatabaseConnection getInstance() {
+        return new DatabaseConnection();
+    }
+
+    private DatabaseConnection() {
+
+    }
+
+    public Connection getConnection() {
+        return null;
+    }
+
+    public void closeConnection() {
+
+    }
+
+    public void executeUpdate(String sql) {
+
+    }
+
+    public ResultSet executeQuery(String sql) {
+        return null;
+    }
+
+    public void closeStatement(Statement statement) {
+
+    }
+
+    public void closePreparedStatement(PreparedStatement) {
+
+    }
+
+    public void closeConnection(Connection connection) {
+
+    }
+
+}
