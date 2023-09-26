62ac5a92a88082e235cd7eee706c2c0a2d5d31b8
Cdric2Leroy <cedric.leroy.psycho@gmail.com>
2023-09-26 14:09:41 +0100

    feat(userinterface): play with Swing and then create three buttons in a label and add them to UserInterface

--- src/main/java/org/example/classes/UserInterface.java
+++ src/main/java/org/example/classes/UserInterface.java
@@ -3,6 +3,8 @@ package org.example.classes;
 import org.example.classes.UserInterfaceComponents.MenuBarBuilder;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -10,18 +12,17 @@ public class UserInterface extends JFrame {
 
 
     public UserInterface() {
+        this.setLayout(new BorderLayout());
 
+        JPanel buttonPannel = new JPanel();
+        buttonPannel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
         //MenuBar
         MenuBarBuilder menuBarBuilder = new MenuBarBuilder();
         this.setJMenuBar(menuBarBuilder);
 
-        JButton playButton = new JButton(
-                "Play");
-
-        //Buttons
-        playButton.setBounds(
-                150, 200, 220,
-                50);
+        // Buttons
+        JButton playButton = new JButton("Play");        //Buttons
+        playButton.setSize(220,50);
         playButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -30,11 +31,42 @@ public class UserInterface extends JFrame {
                 }
             }
         });
-        this.add(playButton);
+        playButton.setAlignmentY(Component.CENTER_ALIGNMENT);
+        buttonPannel.add(playButton);
 
+        JButton stopButton = new JButton("Stop");        //Buttons
+        stopButton.setSize(220,50);
+        stopButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if ("Play".equals(e.getActionCommand())) {
+
+                }
+            }
+        });
+        stopButton.setAlignmentY(Component.CENTER_ALIGNMENT);
+        buttonPannel.add(stopButton);
+
+        JButton pauseButton = new JButton("Pause");        //Buttons
+        pauseButton.setSize(220, 50);
+        pauseButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if ("Play".equals(e.getActionCommand())) {
+
+                }
+            }
+        });
+        pauseButton.setAlignmentY(Component.CENTER_ALIGNMENT);
+        buttonPannel.add(pauseButton);
+
+
+
+        this.add(buttonPannel, BorderLayout.SOUTH);
+        this.pack();
         this.setSize(500, 600);
-        this.setLayout(null);
         this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
 
